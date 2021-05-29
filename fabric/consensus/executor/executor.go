33520b9cb975acdde5807d522f79d0b277b7ec55
ccccccccjl2 <83749796+ccccccccjl2@users.noreply.github.com>
2021-05-29 18:18:21 +0800

    Update executor.go

--- fabric/consensus/executor/executor.go
+++ fabric/consensus/executor/executor.go
@@ -62,22 +62,22 @@ func NewImpl(consumer consensus.ExecutionConsumer, rawExecutor PartialStack, stp
 func (co *coordinatorImpl) ProcessEvent(event events.Event) events.Event {
 	switch et := event.(type) {
 	case executeEvent:
-		logger.Infof("Executor is processing an executeEvent")
+		//logger.Infof("Executor is processing an executeEvent")
 		if co.skipInProgress {
 			logger.Error("FATAL programming error, attempted to execute a transaction during state transfer")
 			return nil
 		}
 
 		if !co.batchInProgress {
-			logger.Infof("Starting new transaction batch")
+			//logger.Infof("Starting new transaction batch")
 			co.batchInProgress = true
 			err := co.rawExecutor.BeginTxBatch(co)
 			_ = err // TODO This should probably panic, see issue 752
 		}
 
-		logger.Infof("ExecTxs")
+		//logger.Infof("ExecTxs")
 		co.rawExecutor.ExecTxs(co, et.txs)
-		logger.Infof("Executed")
+		//logger.Infof("Executed")
 		co.consumer.Executed(et.tag)
 	case commitEvent:
 		logger.Infof("Executor is processing an commitEvent")
