c3b478939be7aaca15e55bc556faa271deb32e9f
ccccccccjl2 <83749796+ccccccccjl2@users.noreply.github.com>
2021-05-30 09:54:24 +0800

    Update tools.go

--- fabric/consensus/pbft/tools.go
+++ fabric/consensus/pbft/tools.go
@@ -483,7 +483,7 @@ func (instance *pbftCore) recvPrepareClerk(prep *Prepare) error {
 				//计票员自己肯定知道结果
 				instance.FeedbackNum++
 				//if instance.FeedbackNum >= len(instance.clerk){
-				if instance.FeedbackNum >= 2{
+				if instance.FeedbackNum >= 1{
 					
 					instance.dealFeedback(feedback)
 				}
@@ -580,7 +580,7 @@ func (instance *pbftCore) dealFeedback(feedback *Feedback) error{
 	instance.stopClerkFeedbackTimer()
 	instance.FeedbackNum++
 	//if instance.FeedbackNum >= len(instance.clerk){
-	if instance.FeedbackNum >= 2{
+	if instance.FeedbackNum >= 1{
 		
 		instance.ifKnowResult = true
 		logger.Infof("replica %d receive enough prepare-feedback", instance.id)
